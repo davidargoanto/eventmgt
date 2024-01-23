5f4645c4bb3a6ff233f6eae3798592d3a6bc7a24
Christopher Darmawirya <christopher.fx45@gmail.com>
2024-01-23 21:40:05 +0900

    Chris code

---
+++ api/auth.js
@@ -0,0 +1,60 @@
+import axios from "axios"
+export async function login(req) {
+
+    const data = await fetch("http://localhost:5670/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: req.email,
+        password: req.password,
+      }),
+    })
+
+    
+    if (data.ok=== true){
+      console.log("hi")
+      const res =  await data.json()
+      console.log (res)
+      localStorage.setItem("token", res.token);
+      return res
+    }else {
+      const res =  await data.json()
+      console.log (res)
+      alert(JSON.stringify(res.error))
+      throw new Error("Network response was not OK");
+
+    };
+
+  }
+  
+  export async function register({ name, email, password }) {
+    const data = await fetch("http://localhost:5670/api/auth/register", {
+      method: "POST",
+  
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }),
+    });
+    return data.json();
+  }
+
+  export async function productUser() {
+    const token = await localStorage.getItem("token");
+  
+    console.log("token => ", token);
+  
+    const res = await axios.get(
+      process.env.NEXT_PUBLIC_API_URL + "/api/product/product-user",
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+    console.log (res)
+    return res
+  }
